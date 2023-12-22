dc9b6b554718b1e74805ef5add890625358a2403
yin-zhi-qi <1853416436@qq.com>
2023-12-22 22:32:41 +0800

    实现自定义主题并完成elePluStyle分支的删除

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 <template>
   <el-button style="--el-button-bg-color: red">Primary</el-button>
   <el-button type="primary">Primary</el-button>
+  <el-button type="warning">Waring</el-button>
 </template>
 
 <style scoped></style>
