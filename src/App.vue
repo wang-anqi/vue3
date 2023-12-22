cd6e09e05ffe223731ab7753623d021046116056
yin-zhi-qi <1853416436@qq.com>
2023-12-22 22:23:17 +0800

    实现elementPlus自定义主题

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <el-button style="--el-button-bg-color: red">Primary</el-button>
+  <el-button type="primary">Primary</el-button>
 </template>
 
 <style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,36 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+//elementPlus自定义对应包
+import ElementPlus from 'unplugin-element-plus/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+
     AutoImport({
       resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })]
+    }),
+    //自定义elementPlus的sass格式
+    ElementPlus({
+      useSource: true
     })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  //自定义elementPlus的sass格式
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`
+      }
     }
   }
 })
