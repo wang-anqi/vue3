7330ed48799170a61219d01bbdcd0d10da32cdc6
yin-zhi-qi <1853416436@qq.com>
2023-12-24 20:49:37 +0800

    登录页基本框架

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <script setup></script>
 
 <template>
-  <el-button style="--el-button-bg-color: red">Primary</el-button>
-  <el-button type="primary">Primary</el-button>
-  <el-button type="warning">Waring</el-button>
-
   <RouterView></RouterView>
 </template>
 
