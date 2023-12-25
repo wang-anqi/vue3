905a19202e9f7042e3021ae407bd6d55849c4c34
yin-zhi-qi <1853416436@qq.com>
2023-12-25 21:17:29 +0800

    进展到最后个人部分

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -1,2 +1,67 @@
-<script setup></script>
-<template>文章channel</template>
+<script setup>
+import { Edit, Delete } from '@element-plus/icons-vue'
+import pageContainer from '@/components/PageContainer.vue'
+import channelEdit from './components/ChannelEdit.vue'
+import { ref } from 'vue'
+import { artGetChannelsService } from '@/api/article'
+const channelList = ref([])
+const loading = ref(false)
+
+const getChannelList = async () => {
+  loading.value = true
+  const res = await artGetChannelsService()
+  channelList.value = res.data.data
+  loading.value = false
+}
+getChannelList()
+
+const onDelChannel = (row) => {
+  console.log(row)
+}
+const dialog = ref()
+const onEditChannel = (row) => {
+  dialog.value.open(row)
+}
+const onAddChannel = () => {
+  dialog.value.open({})
+}
+//父组件对子组件的监听相应
+const onSuccess = () => {
+  getChannelList()
+}
+</script>
+<template>
+  <page-container title="文章分类">
+    <template #extra>
+      <el-button type="primary" @click="onAddChannel"> 添加分类 </el-button>
+    </template>
+
+    <el-table :data="channelList" v-loading="loading" style="width: 100%">
+      <el-table-column label="序号" width="100" type="index"> </el-table-column>
+      <el-table-column label="分类名称" prop="cate_name"></el-table-column>
+      <el-table-column label="分类别名" prop="cate_alias"></el-table-column>
+      <el-table-column label="操作" width="100">
+        <template #default="{ row }">
+          <el-button
+            :icon="Edit"
+            circle
+            plain
+            type="primary"
+            @click="onEditChannel(row)"
+          ></el-button>
+          <el-button
+            :icon="Delete"
+            circle
+            plain
+            type="danger"
+            @click="onDelChannel(row)"
+          ></el-button>
+        </template>
+      </el-table-column>
+      <template #empty>
+        <el-empty description="没有数据" />
+      </template>
+    </el-table>
+    <channel-edit ref="dialog" @success="onSuccess"></channel-edit>
+  </page-container>
+</template>
--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -1,2 +1,155 @@
-<script setup></script>
-<template>文章manage</template>
+<script setup>
+import pageContainer from '@/components/PageContainer.vue'
+import { Delete, Edit } from '@element-plus/icons-vue'
+import channelSelect from './components/ChannelSelect.vue'
+import articleEdit from './components/ArticleEdit.vue'
+import { artGetListService, artDelService } from '@/api/article'
+import { formatTime } from '@/utils/format'
+import { ref } from 'vue'
+
+const loading = ref(false)
+
+// 数据
+const articleList = ref([])
+const total = ref(0)
+const getArticleList = async () => {
+  loading.value = true
+  const res = await artGetListService(params.value)
+  articleList.value = res.data.data
+  total.value = res.data.total
+  loading.value = false
+}
+getArticleList()
+
+const onDeleteArticle = async (row) => {
+  await ElMessageBox.confirm('你确认删除该文章信息吗？', '温馨提示', {
+    type: 'warning',
+    confirmButtonText: '确认',
+    cancelButtonText: '取消'
+  })
+  await artDelService(row.id)
+  ElMessage({ type: 'success', message: '删除成功' })
+  getArticleList()
+}
+const params = ref({
+  pagenum: 1,
+  pagesize: 5,
+  cate_id: '',
+  state: ''
+})
+//弹窗的显示
+const onAddArticle = () => {
+  articleEditRef.value.open({})
+}
+const onEditArticle = (row) => {
+  articleEditRef.value.open(row)
+}
+
+//提供分页逻辑
+const onSizeChange = (size) => {
+  params.value.pagenum = 1
+  params.value.pagesize = size
+  getArticleList()
+}
+const onCurrentChange = (page) => {
+  params.value.pagenum = page
+  getArticleList()
+}
+
+//搜索 重置
+const onSearch = () => {
+  params.value.pagenum = 1
+  getArticleList()
+}
+
+const onReset = () => {
+  params.value.pagenum = 1
+  params.value.cate_id = ''
+  params.value.state = ''
+  getArticleList()
+}
+const articleEditRef = ref()
+
+//添加文本
+// 添加修改成功
+const onSuccess = (type) => {
+  if (type === 'add') {
+    // 如果是添加，需要跳转渲染最后一页，编辑直接渲染当前页
+    const lastPage = Math.ceil((total.value + 1) / params.value.pagesize)
+    params.value.pagenum = lastPage
+  }
+  getArticleList()
+}
+</script>
+<template>
+  <page-container title="文章管理">
+    <template #extra>
+      <el-button type="primary" @click="onAddArticle"> 发布文章 </el-button>
+    </template>
+
+    <el-form inline>
+      <el-form-item label="文章分类：">
+        <channel-select v-model="params.cate_id"></channel-select>
+      </el-form-item>
+      <el-form-item label="发布状态：" v-model="params.state">
+        <el-select>
+          <el-option label="已发布" value="已发布"></el-option>
+          <el-option label="草稿" value="草稿"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="onSearch" type="primary">搜索</el-button>
+        <el-button @click="onReset">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table v-loading="loading" :data="articleList" style="width: 100%">
+      <el-table-column label="文章标题" width="400">
+        <template #default="{ row }">
+          <el-link type="primary" :underline="false">{{ row.title }}</el-link>
+        </template>
+      </el-table-column>
+      <el-table-column label="分类" prop="cate_name"></el-table-column>
+      <el-table-column label="发表时间">
+        <template #default="{ row }">
+          {{ formatTime(row.pub_date) }}
+        </template>
+      </el-table-column>
+      <el-table-column label="状态" prop="state"></el-table-column>
+      <el-table-column label="操作" width="100">
+        <template #default="{ row }">
+          <el-button
+            :icon="Edit"
+            circle
+            plain
+            type="primary"
+            @click="onEditArticle(row)"
+          ></el-button>
+          <el-button
+            :icon="Delete"
+            circle
+            plain
+            type="danger"
+            @click="onDeleteArticle(row)"
+          ></el-button>
+        </template>
+      </el-table-column>
+      <template #empty>
+        <el-empty description="没有数据" />
+      </template>
+    </el-table>
+    <!-- 分页区域 -->
+    <el-pagination
+      v-model:current-page="params.pagenum"
+      v-model:page-size="params.pagesize"
+      :page-sizes="[2, 3, 4, 5, 10]"
+      layout="jumper, total, sizes, prev, pager, next"
+      background
+      :total="total"
+      @size-change="onSizeChange"
+      @current-change="onCurrentChange"
+      style="margin-top: 20px; justify-content: flex-end"
+    />
+    <!-- 添加编辑的抽屉 -->
+    <article-edit ref="articleEditRef" @success="onSuccess"></article-edit>
+  </page-container>
+</template>
