a183fadbf392cd6581c4fa1b3af016277f75ab0f
yin-zhi-qi <1853416436@qq.com>
2023-12-25 21:28:29 +0800

    over

--- src/api/user.js
+++ src/api/user.js
@@ -7,3 +7,10 @@ export const userLoginService = ({ username, password }) =>
   request.post('api/login', { username, password })
 
 export const userGetInfoService = () => request.get('/my/userinfo')
+//更新个人信息
+export const userUpdateInfoService = ({ id, nickname, email }) =>
+  request.put('/my/userinfo', { id, nickname, email })
+export const userUploadAvatarService = (avatar) =>
+  request.patch('/my/update/avatar', { avatar })
+export const userUpdatePassService = ({ old_pwd, new_pwd, re_pwd }) =>
+  request.patch('/my/updatepwd', { old_pwd, new_pwd, re_pwd })
