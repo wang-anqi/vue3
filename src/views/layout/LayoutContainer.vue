1a68263c0c56bcc3573458c86845b27442932ac5
yin-zhi-qi <1853416436@qq.com>
2023-12-25 22:01:20 +0800

    修改小细节

--- src/views/layout/LayoutContainer.vue
+++ src/views/layout/LayoutContainer.vue
@@ -94,7 +94,7 @@ const handleCommand = async (key) => {
     <el-container>
       <el-header>
         <div>
-          黑马程序员：<strong>{{
+          程序员：<strong>{{
             userStore.user.nickname || userStore.user.username
           }}</strong>
         </div>
@@ -127,7 +127,7 @@ const handleCommand = async (key) => {
       <el-main>
         <router-view></router-view>
       </el-main>
-      <el-footer>大事件 ©2023 Created by 黑马程序员</el-footer>
+      <el-footer>大事件 ©2023 Created by anqiW</el-footer>
     </el-container>
   </el-container>
 </template>
